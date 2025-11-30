1d890539d56e5b702569974ce39f5723520b8b8a
KaviduDinal <dinalkavidu5@gmail.com>
2025-11-30 22:13:22 +0530

    add changes  with buttonsan dplayer

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import {songsData} from '../assets/assets'
+import {songsData, assets} from '../assets/assets'
 
 const Player = () => {
   return (
-    <div className='h-[10%] bg-black flex justify-center items-center text-white px-4' >
-        <div className='hidden lg:flex items-center gap-4'>
-            <img className='w-12' src={songsData[0].image} alt=""/>
+    <div className='fixed bottom-0 left-0 w-full h-16 bg-black flex items-center justify-start text-white px-4 z-50'>
+        <div className='flex items-center gap-4'>
+            <img className='w-12 h-12 object-cover' src={songsData[0].image} alt={songsData[0].name} />
+            <div className='flex flex-col'>
+                <p className='text-sm font-medium'>{songsData[0].name}</p>
+                <p className='text-xs text-gray-300'>{songsData[0].desc}</p>
+            </div>
         </div>
-        <p>{songsData[0].name}</p>
-        <p>{songsData[0].desc}</p>
+        <div className='flex flex-col items-center gap-1 m-auto'>
+            <div className='flex gap-4'>
+                <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
+                <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+                <img className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+                <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
+                <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
+
+            </div>
+            <div className='flex items-center gap-5'>
+                <p>1.06</p>
+                <div className='w-[60vw] max-w-[500px ] bg-gray-300 rounded-full cursor-pointer'>
+                    <hr className='h-1 border-none w-50 bg-green-800 ronded-full'/>
+                </div>
+                <p>3.45</p> 
+
+            </div>
+        </div>
+        <div className='hidden lg:flex items-cnter gap-2 opacity-75'>
+            <img className='w-4' src='{assets.plays_icon}' alt=''/>
+            
+
+        </div>
     </div>
-    
   )
 }
 
