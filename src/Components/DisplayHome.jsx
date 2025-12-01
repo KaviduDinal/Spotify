5450610596b0570e7f69d6f96542cbfa4aaaf05f
KaviduDinal <dinalkavidu5@gmail.com>
2025-12-01 23:43:38 +0530

    Add the music  list Album and song items well.

--- src/Components/DisplayHome.jsx
+++ src/Components/DisplayHome.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, albumsData, songsData } from '../assets/assets'
 import Navbar from './Navbar'
+import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className='mt-4'>
+        <h2 className='text-white text-2xl font-bold mb-4'>Featured Albums</h2>
+        <div className='flex space-x-3 overflow-x-auto py-2 px-2'>
+          {albumsData.map((item) => (
+            <AlbumItem key={item.id} image={item.image} name={item.name} desc={item.desc} id={item.id} />
+          ))}
+        </div>
+      </div>
+
+      <div className='mt-4'>
+        <h2 className='text-white text-2xl font-bold mb-4'>Today's biggest hits</h2>
+        <div className='flex space-x-3 overflow-x-auto py-2 px-2'>
+          {songsData.map((item,index)=>(<SongItem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id}/>))}
+        </div>
+
+      </div>
     </>
   )
 }
