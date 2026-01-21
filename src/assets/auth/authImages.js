f84ae2d88fff16c2d9a73de1b1300dafd7e9c339
KaviduDinal <dinalkavidu5@gmail.com>
2026-01-21 18:30:42 +0530

    Basically completely finished authentication

---
+++ src/assets/auth/authImages.js
@@ -0,0 +1,15 @@
+import bg from "./bg.png";
+import artist1 from "./artist1.jpg";
+import artist2 from "./artist2.jpg";
+import artist3 from "./artist3.jpg";
+import artist4 from "./artist4.jpg";
+import artist5 from "./artist5.jpg";
+
+export {
+  bg,
+  artist1,
+  artist2,
+  artist3,
+  artist4,
+  artist5,
+};
