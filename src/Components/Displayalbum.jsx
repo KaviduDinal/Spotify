a96ed78b147157f063079cbcedcbd36c5d9cfa72
KaviduDinal <dinalkavidu5@gmail.com>
2026-01-23 11:30:00 +0530

    Song sec change

--- src/Components/Displayalbum.jsx
+++ src/Components/Displayalbum.jsx
@@ -37,7 +37,7 @@ const Displayalbum = () => {
                         1,231,594 likes
                         <span className='mx-2'>●</span>
                         <b>100 songs,</b>
-                        <span className='ml-2'>about 8 hr 30 min</span>
+                        <span className='ml-2'>about 8 hr 40 min</span>
                     </p>
                 </div>
             </div>
