02aed314f7f99dc69d30720a78f14a69115b70b8
KaviduDinal <dinalkavidu5@gmail.com>
2026-02-09 16:56:47 +0530

    chceck the progress of the system and  mak  simple mistake correctly

--- src/Components/DisplayHome.jsx
+++ src/Components/DisplayHome.jsx
@@ -9,7 +9,7 @@ const DisplayHome = () => {
     <>
       <Navbar />
       <div className='mt-4'>
-        <h2 className='text-white text-2xl font-bold mb-4'>Featured Albums</h2>
+        <h2 className='text-white text-2xl font-bold mb-4'>Featured Albums.</h2>
         <div className='flex space-x-3 overflow-x-auto py-2 px-2'>
           {albumsData.map((item) => (
             <AlbumCard key={item.id} image={item.image} name={item.name} desc={item.desc} id={item.id} bgColor={item.bgColor} />
